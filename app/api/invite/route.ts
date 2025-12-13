94ee6b800211616b43a03c3cbf4926bf1b46e1e3
Naim Ahmad <naimahmad201103@proton.me>
2025-12-13 10:11:21 +0600

    invite api completed

--- app/api/invite/route.ts
+++ app/api/invite/route.ts
@@ -1,15 +1,21 @@
 import { createClient } from "@supabase/supabase-js";
+import { headers } from "next/headers";
 import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   const { email } = await request.json();
+  const nextHeaders = await headers();
 
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.SUPABASE_SERVICE_ROLE_KEY!
   );
 
-  const { data, error } = await supabase.auth.admin.inviteUserByEmail(email);
+  const APP_URL = process.env.NEXT_PUBLIC_APP_URL ?? nextHeaders.get("origin");
+
+  const { data, error } = await supabase.auth.admin.inviteUserByEmail(email, {
+    redirectTo: `${APP_URL}/account-request`,
+  });
 
   if (error) {
     return NextResponse.json(
