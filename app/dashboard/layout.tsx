0efd685ba9a1f4e965f706096f5d56a709a10bd5
Naim Ahmad <naimahmad201103@proton.me>
2026-01-01 23:57:40 +0600

    contact header fixed

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,166 +1,18 @@
 "use client";
 
-import LogoutButton from "@/components/auth/LogoutButton";
 import LeftSidebar from "@/components/dashboard/LeftSidebar";
 import RightSidebar from "@/components/dashboard/RightSidebar";
-import { ModeToggle } from "@/components/ModeToggle/ModeToggle";
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import { Input } from "@/components/ui/input";
-import { useUser } from "@/providers/UserProvider";
-import { Search } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import { useRouter } from "next/navigation";
+import Header from "@/components/Header/Header";
 
 export default function DashboardLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const router = useRouter();
-  const { user } = useUser();
-
   return (
     <div className="h-screen w-screen flex flex-col bg-background text-gray-800 dark:bgbackround dark:text-gray-100 overflow-hidden">
       {/* Header */}
-      <header className="hidden lg:flex items-center h-28 justify-between border-b bg-white px-6 py-3 shadow-sm dark:bg-neutral-800 shrink-0">
-        <div className="flex items-center gap-3">
-          <Link href={`/dashboard`}>
-            <div className="flex items-center gap-3">
-              <Image
-                src="/images/FLLogo.svg"
-                alt="FL Logo"
-                width={90}
-                height={90}
-                quality={100}
-                className="rounded-full"
-              />
-              <h1 className="text-lg md:text-[1.5rem] font-bold text-[#63a053]">
-                Gestionnaires
-              </h1>
-            </div>
-          </Link>
-          <div className="h-10 w-[0.3px] bg-[#000000] mx-2" />
-          <div className="relative w-12 h-12 rounded-full overflow-hidden">
-            <Image
-              src="/images/christina.jpg"
-              alt="User photo"
-              width={65}
-              height={65}
-              className="object-cover"
-              sizes="65px"
-            />
-          </div>
-          {user && (
-            <div className="flex flex-col">
-              <span className="text-[1.125rem] font-medium leading-none">
-                {user?.fullName || ""}
-              </span>
-              <span className="text-[1rem] leading-none text-muted-foreground">
-                {user.email}
-              </span>
-            </div>
-          )}
-        </div>
-
-        <div className="flex items-center gap-[15px]">
-          {/* Search */}
-          <div className="hidden md:block relative w-80">
-            <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-[#63a053] h-4 w-4" />
-            <Input
-              type="text"
-              placeholder="Candidat, contact, activité, étiquette...ect"
-              className="pl-9 h-10 bg-gray-100 border-0 text-sm text-[#A4A4A4] placeholder:text-gray-500 focus-visible:ring-1 focus-visible:ring-[#63a053]"
-            />
-          </div>
-
-          {/* Dropdown Trigger */}
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button
-                style={{
-                  padding: "10px",
-                }}
-                className="bg-[#63a053] hover:bg-[#528a45] rounded-xs cursor-pointer text-white text-sm  w-10 h-8 flex items-center gap-1"
-              >
-                <svg
-                  className="w-full h-full"
-                  viewBox="0 0 26 14"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M1.15039 1.15002H24.1504"
-                    stroke="white"
-                    strokeWidth="2.3"
-                    strokeLinecap="round"
-                  />
-                  <path
-                    d="M1.15039 6.65002H24.1504"
-                    stroke="white"
-                    strokeWidth="2.3"
-                    strokeLinecap="round"
-                  />
-                  <path
-                    d="M1.15039 12.15H24.1504"
-                    stroke="white"
-                    strokeWidth="2.3"
-                    strokeLinecap="round"
-                  />
-                </svg>
-              </Button>
-            </DropdownMenuTrigger>
-
-            <DropdownMenuContent
-              align="end"
-              sideOffset={6}
-              className="w-44 bg-white dark:bg-neutral-800 border dark:border-neutral-700 shadow-md rounded-none"
-            >
-              {/* account requests */}
-              <DropdownMenuItem className="cursor-pointer rounded-xs focus:bg-[#326EA6] focus:text-white">
-                <Link href="/dashboard/account-requests">
-                  Demande de compte
-                </Link>
-              </DropdownMenuItem>
-
-              <Link href="/dashboard">
-                <DropdownMenuItem className="cursor-pointer rounded-xs focus:bg-[#326EA6] focus:text-white">
-                  Projet
-                </DropdownMenuItem>
-              </Link>
-
-              <Link href="/contact">
-                <DropdownMenuItem className="cursor-pointer rounded-xs focus:bg-[#326EA6] focus:text-white">
-                  Contact
-                </DropdownMenuItem>
-              </Link>
-
-              {/* <DropdownMenuItem
-                onClick={() => router.push("/profile")}
-                className="cursor-pointer rounded-xs focus:bg-[#326EA6] focus:text-white"
-              >
-                Profil
-              </DropdownMenuItem> */}
-
-              <div className="h-px bg-gray-300 my-1"></div>
-
-              {/* logout button */}
-
-              <DropdownMenuItem className="cursor-pointer rounded-xs transition text-red-600 focus:bg-red-600/10 focus:text-red-600 dark:focus:bg-red-600/20">
-                <LogoutButton />
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-
-          <ModeToggle />
-        </div>
-      </header>
+      <Header />
 
       {/* Main layout */}
       <main className="flex w-screen h-screen bg-[#ebebeb] dark:bg-card overflow-hidden">
--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,8 +1,10 @@
 "use client";
 
-import { MenuIcon, Plus, Search } from "lucide-react";
+import { useUser } from "@/providers/UserProvider";
+import { Search } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
+import LogoutButton from "../auth/LogoutButton";
 import { ModeToggle } from "../ModeToggle/ModeToggle";
 import { Button } from "../ui/button";
 import {
@@ -14,88 +16,137 @@ import {
 import { Input } from "../ui/input";
 
 const Header = () => {
+  const { user } = useUser();
+
   return (
-    <header className="flex items-center justify-between border-b bg-white px-6 py-3 shadow-sm dark:bg-neutral-800 shrink-0">
+    <header className="hidden lg:flex items-center h-28 justify-between border-b bg-white px-6 py-3 shadow-sm dark:bg-neutral-800 shrink-0">
       <div className="flex items-center gap-3">
         <Link href={`/dashboard`}>
           <div className="flex items-center gap-3">
             <Image
-              src="/images/Logo FL.png"
+              src="/images/FLLogo.svg"
               alt="FL Logo"
-              width={72}
-              height={70}
+              width={90}
+              height={90}
               quality={100}
               className="rounded-full"
             />
-            <h1 className="text-lg md:text-xl font-bold text-[#63a053]">
+            <h1 className="text-lg md:text-[1.5rem] font-bold text-[#63a053]">
               Gestionnaires
             </h1>
           </div>
         </Link>
         <div className="h-10 w-[0.3px] bg-[#000000] mx-2" />
-        <div className="relative w-[37px] h-[37px] rounded-full overflow-hidden">
+        <div className="relative w-12 h-12 rounded-full overflow-hidden">
           <Image
-            src="/images/Photo ID.png"
+            src="/images/christina.jpg"
             alt="User photo"
-            fill
+            width={65}
+            height={65}
             className="object-cover"
-            sizes="37px"
+            sizes="65px"
           />
         </div>
+        {user && (
+          <div className="flex flex-col">
+            <span className="text-[1.125rem] font-medium leading-none">
+              {user?.fullName || ""}
+            </span>
+            <span className="text-[1rem] leading-none text-muted-foreground">
+              {user.email}
+            </span>
+          </div>
+        )}
       </div>
 
-      <div className="flex items-center gap-3">
+      <div className="flex items-center gap-[15px]">
         {/* Search */}
         <div className="hidden md:block relative w-80">
           <Search className="absolute left-3 top-1/2 -translate-y-1/2 text-[#63a053] h-4 w-4" />
           <Input
             type="text"
             placeholder="Candidat, contact, activité, étiquette...ect"
-            className="pl-9 bg-gray-100 border-0 text-sm placeholder:text-gray-500 focus-visible:ring-1 focus-visible:ring-[#63a053]"
+            className="pl-9 h-10 bg-gray-100 border-0 text-sm text-[#A4A4A4] placeholder:text-gray-500 focus-visible:ring-1 focus-visible:ring-[#63a053]"
           />
         </div>
 
         {/* Dropdown Trigger */}
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <Button className="bg-[#63a053] hover:bg-[#528a45] rounded-xs cursor-pointer text-white text-sm px-4 py-2 flex items-center gap-1">
-              <Plus className="w-4 h-4" /> Ajouter
+            <Button
+              style={{
+                padding: "10px",
+              }}
+              className="bg-[#63a053] hover:bg-[#528a45] rounded-xs cursor-pointer text-white text-sm  w-10 h-8 flex items-center gap-1"
+            >
+              <svg
+                className="w-full h-full"
+                viewBox="0 0 26 14"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M1.15039 1.15002H24.1504"
+                  stroke="white"
+                  strokeWidth="2.3"
+                  strokeLinecap="round"
+                />
+                <path
+                  d="M1.15039 6.65002H24.1504"
+                  stroke="white"
+                  strokeWidth="2.3"
+                  strokeLinecap="round"
+                />
+                <path
+                  d="M1.15039 12.15H24.1504"
+                  stroke="white"
+                  strokeWidth="2.3"
+                  strokeLinecap="round"
+                />
+              </svg>
             </Button>
           </DropdownMenuTrigger>
 
           <DropdownMenuContent
             align="end"
             sideOffset={6}
-            className="w-44 bg-white dark:bg-neutral-800 border dark:border-neutral-700 shadow-md rounded-md"
+            className="w-44 bg-white dark:bg-neutral-800 border dark:border-neutral-700 shadow-md rounded-none"
           >
-            <DropdownMenuItem
-              onClick={() => console.log("Add Task")}
-              className="cursor-pointer focus:bg-[#326EA6] focus:text-white"
-            >
-              Tâche
+            {/* account requests */}
+            <DropdownMenuItem className="cursor-pointer rounded-xs focus:bg-[#326EA6] focus:text-white">
+              <Link href="/dashboard/account-requests">Demande de compte</Link>
             </DropdownMenuItem>
-            <DropdownMenuItem
-              onClick={() => console.log("Add Project")}
-              className="cursor-pointer focus:bg-[#326EA6] focus:text-white"
-            >
-              Projet
-            </DropdownMenuItem>
+
+            <Link href="/dashboard">
+              <DropdownMenuItem className="cursor-pointer rounded-xs focus:bg-[#326EA6] focus:text-white">
+                Projet
+              </DropdownMenuItem>
+            </Link>
+
             <Link href="/contact">
-              <DropdownMenuItem className="cursor-pointer focus:bg-[#326EA6] focus:text-white">
+              <DropdownMenuItem className="cursor-pointer rounded-xs focus:bg-[#326EA6] focus:text-white">
                 Contact
               </DropdownMenuItem>
             </Link>
-            <DropdownMenuItem
-              onClick={() => console.log("Add Dossier")}
-              className="cursor-pointer focus:bg-[#326EA6] focus:text-white"
-            >
-              Dossier
+
+            {/* <DropdownMenuItem
+                onClick={() => router.push("/profile")}
+                className="cursor-pointer rounded-xs focus:bg-[#326EA6] focus:text-white"
+              >
+                Profil
+              </DropdownMenuItem> */}
+
+            <div className="h-px bg-gray-300 my-1"></div>
+
+            {/* logout button */}
+
+            <DropdownMenuItem className="cursor-pointer rounded-xs transition text-red-600 focus:bg-red-600/10 focus:text-red-600 dark:focus:bg-red-600/20">
+              <LogoutButton />
             </DropdownMenuItem>
           </DropdownMenuContent>
         </DropdownMenu>
 
         <ModeToggle />
-        <MenuIcon />
       </div>
     </header>
   );
