e7c1c55f8efe56b3946f817bf0e51abecf745d18
Shohaib hossain <khanshohaibhossain@gmail.com>
2026-01-11 21:53:19 +0600

    added some padding

--- app/dashboard/[id]/project/[projectId]/MilestoneTabas.tsx
+++ app/dashboard/[id]/project/[projectId]/MilestoneTabas.tsx
@@ -47,7 +47,7 @@ export function MilestoneTabs({
       >
         <span
           onClick={() => onChange(m)}
-          className="bg-[#A2CF96] py-1.5 px-3 text-white cursor-pointer"
+          className="bg-[#A2CF96] px-3 py-2.5 text-white cursor-pointer"
         >
           {ind + 1}.
         </span>
@@ -60,7 +60,7 @@ export function MilestoneTabs({
               onChange(m);
             }
           }}
-          className="py-1.5 px-2 text-white capitalize cursor-pointer text-[0.875rem]"
+          className="p-2.5 text-white capitalize cursor-pointer text-[0.875rem]"
         >
           {m.title}
         </span>
